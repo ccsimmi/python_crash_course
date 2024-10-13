f59a308675ea08ee93ff0fb5f8244833d0ad70cb
Sim <ffsimmi@gmail.com>
2024-10-13 11:48:21 +0100

    chapter 1 python crash course

---
+++ chapter_2/variables.py
@@ -0,0 +1,26 @@
+message = "Hello Python"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(type(name))
+
+first_name = "Sim"
+last_name = "Sim"
+# Interpolation is different
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+cubed = 5 ** 3
+
+floaty = 0.2 + 0.2
+net_worth = 14_000_000_000
+
+# multiple assignments
+x , y , z = 0, 0, 0
+
+# No built in constant, but indicate to not change
+BUILT_IN_CONSTANT = "I am a constant"
+
+#import this
+#print(this)
